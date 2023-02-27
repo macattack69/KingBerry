1db7ce542024b3404cf75729ccc1e879aa8cfe8a
macattack69 <michaelryanschoeneich@gmail.com>
2023-02-26 20:38:03 -0600

    This is CRAZY!!! PRESS PLAY
    I don't think the movement part is working, there are some errors in the script but you can look around :O🤠

--- README.md
+++ README.md
@@ -3,5 +3,4 @@ King Barry Jame
 This is the readme for the KingBerry stuff. Currently this project could 
 go many ways, but it will likely be epicsauce
 
-I have straight up copied a demo from the website and pasted it in here to 
-learn with. I'm really good at it too, sue me.
+I deleted the test project I put in here. I figured out what I wanted to know. 
---
+++ kingbarry/ReadyPlayerOne.gd
@@ -0,0 +1,51 @@
+#ChatGPT Wrote this, so let me add some comments to try to explain it
+#these notes are for my benefit, but enjoy them regardless🤠
+#Michael
+extends KinematicBody
+#so this is at the start of all the scripts to show their parent class
+#specifying this class provides customizable functionality
+#KinematicBody comes barebones from Godot. It only is significant
+#because it can listen for input, unlike other bodies, which listen
+#to the physics engine for their physics. We have to define the physics for it here,
+#and use the input to utilize said physics.
+
+export var speed = 10
+#clearly this goes brrrrrr
+#its our only variable for now, and like most languages variables
+#are preferrably specified before functions because multiple functions
+#typically call the same variables and you might get some undefined errors if
+#you structured it differently.
+
+func _ready():
+	set_process_input(true)
+#this turns listening for input on without this the kinematicBody player wouldnt take input
+#this happens when the parent node is first placed in the scene
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_x(deg2rad(-event.relative.y))
+		$Camera.rotate_y(deg2rad(-event.relative.x))
+#this rotates the camera on mouse movement. Relative x and y above
+
+func _physics_process(delta):
+	var direction = Vector3()
+	if Input.is_action_pressed("move_forward"):
+		direction += -transform.basis.z
+	if Input.is_action_pressed("move_backward"):
+		direction += transform.basis.z
+	if Input.is_action_pressed("move_left"):
+		direction += -transform.basis.x
+	if Input.is_action_pressed("move_right"):
+		direction += transform.basis.x
+	direction = direction.normalized()
+	direction *= speed
+	move_and_slide(direction)
+#this is the big one, like the last function takes mouse input (only movement for now)
+#this function takes keyboard input (or gamepad input idk yet)
+#this is pretty cool, basically its taking a direction variable,
+#its checking to see if the button is pressed
+#then its taking that variable, adding a positive or negative value
+#it then makes sure that the direction is normalized (more on that later)
+#it then uses a combination of the speed variable from earlier, and the built-in
+#move_and_slide() function to manipulate the parent nodes movement
+#pretttty neaaat right?

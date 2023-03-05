57bf418974c23bdae0b3dd755920175e8ba130f8
AvianFIu <51840255+AvianFIu@users.noreply.github.com>
2023-03-05 09:36:56 -0600

    gravity goes down

--- kingbarry/ReadyPlayerOne.gd
+++ kingbarry/ReadyPlayerOne.gd
@@ -1,13 +1,4 @@
-#ChatGPT Wrote this, so let me add some comments to try to explain it
-#these notes are for my benefit, but enjoy them regardless🤠
-#Michael
 extends KinematicBody
-#so this is at the start of all the scripts to show their parent class
-#specifying this class provides customizable functionality
-#KinematicBody comes barebones from Godot. It only is significant
-#because it can listen for input, unlike other bodies, which listen
-#to the physics engine for their physics. We have to define the physics for it here,
-#and use the input to utilize said physics.
 
 #beginning work on gravity
 const GRAVITY = Vector3(0,10,0)
@@ -24,9 +15,6 @@ onready var camera = $Camera
 func _ready():
 	set_process_input(true)
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-#this turns listening for input on without this the kinematicBody player wouldnt take input
-#this happens when the parent node is first placed in the scene
-#hides mouse, locks to application
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -56,27 +44,10 @@ func _physics_process(delta):
 	direction *= speed
 	move_and_slide(direction)
 	# apply gravity
-	VELOCITY += GRAVITY * delta
+	VELOCITY -= GRAVITY * delta
 	VELOCITY = move_and_slide(VELOCITY, Vector3.UP)
 	move_and_slide(direction + VELOCITY)
 	var snap = Vector3(0, -1, 0) # snap to the floor
 	var floor_normal = Vector3()
 	var is_on_floor_new = move_and_slide(VELOCITY + snap, Vector3(0, 1, 0), false, 4, 0.785398, true)
-
-#this is the big one, like the last function takes mouse input (only movement for now)
-#this function takes keyboard input (or gamepad input idk yet)
-#this is pretty cool, basically its taking a direction variable,
-#its checking to see if the button is pressed
-#then its taking that variable, adding a positive or negative value
-#it then makes sure that the direction is normalized (more on that later)
-#it then uses a combination of the speed variable from earlier, and the built-in
-#move_and_slide() function to manipulate the parent nodes movement
-#pretttty neaaat right?
-
-#interesting note now that I made the movement function above work
-#basically, the "move_..." strings are input mapping actions dictated by
-#keys, in that same mentioned input mapping menu under project settings
-#thanks debugga! I do not use the hard R
-
-
 
